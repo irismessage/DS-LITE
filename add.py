f18c0bbac3c1bb6779f96a87b42704992233f5c5
jmcb <joelsgp@protonmail.com>
2023-11-18 17:18:48 +0000

    Bug fi

--- add.py
+++ add.py
@@ -8,11 +8,12 @@ PATH_IN_FILE = "0-games.txt"
 
 def add(target: str):
     with open(PATH_IN_FILE, "a") as in_file:
-        in_file.writeline(target + "\n")
+        in_file.write(target + "\n")
 
 
 def main():
-    target = Path(argv[1]).name
+    target = Path(argv[1]).stem
+    add(target)
 
 
 if __name__ == "__main__":
