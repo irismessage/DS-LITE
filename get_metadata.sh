6eb36440293950ee1801d1ed2fba8f5392c0a8cc
jmcb <joelsgp@protonmail.com>
2023-11-18 16:06:51 +0000

    Latest metadata

--- get_metadata.sh
+++ get_metadata.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://archive.org/metadata/no-ndsdec2021 | python -m json.tool - metadata.json
+curl https://archive.org/metadata/no-ndsdec2022 | python -m json.tool - metadata.json
