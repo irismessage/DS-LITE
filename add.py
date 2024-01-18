84d019e914119dc359e350e7cc02eddbd505deb5
jmcb <joelsgp@protonmail.com>
2024-01-18 22:18:01 +0000

    Don't add games twice

--- add.py
+++ add.py
@@ -3,12 +3,30 @@
 from pathlib import Path
 from sys import argv
 
-PATH_IN_FILE = "0-games.txt"
+PATH_IN_FILE = Path("0-games.txt")
+
+
+def load() -> set[str]:
+    with open(PATH_IN_FILE) as in_file:
+        games = set(in_file.readlines())
+    return games
+
+
+def save(old: set[str], new: set[str]):
+    added = new - old
+    with open(PATH_IN_FILE, "a") as in_file:
+        in_file.writelines(added)
 
 
 def add(target: str):
+    target = target + "\n"
+    games = load()
+    if target in games:
+        print("Already in there")
+        return
+
     with open(PATH_IN_FILE, "a") as in_file:
-        in_file.write(target + "\n")
+        in_file.write()
 
 
 def main():
