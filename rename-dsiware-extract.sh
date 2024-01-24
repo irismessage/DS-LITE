06bfa555ff2d8b1a0f31c91eab7553a170816572
jmcb <joelsgp@protonmail.com>
2024-01-24 21:05:13 +0000

    test extract renaming

---
+++ rename-dsiware-extract.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eux
+
+dest="${1}"
+file="${2}"
+stem="$(basename "${file}" '.7z')"
+out="${stem}.nds"
+
+# if dsiware rip contents is just like 00000002,
+# replace it with zip name
+# idk why no-intro allows that at all
+bsdtar \
+    --keep-old-files \
+    --directory "${dest}" \
+    -s "/[0-9]+/${out}/" \
+    -xf "${file}"
