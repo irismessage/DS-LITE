beb5329807ce372aeb77ce6c688e0207fa109dd9
jmcb <joelsgp@protonmail.com>
2024-02-03 13:15:44 +0000

    Notes on screenshots

--- image-resize.sh
+++ image-resize.sh
@@ -5,7 +5,16 @@ input="${1}"
 # output="${input%.*}.png"
 output="${2}"
 
-size="208x156"
+# size for twilightmenu images
+size='208x156'
+# size for nds-bootstrap screenshots
+#size='256x192'
+# arguments for nds-bootstrap screenshots
+ndsbargs='-colorspace sRGB -depth 8'
+
+# best thing to do in the long term
+# rather than placing custom images in screenshots.tar
+# is to extract and convert screenshots.tar into theme images
 
 convert \
     "${input}" \
