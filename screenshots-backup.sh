3c778c2d5e7d8fbe34b44d245bec5f12fd4e56c9
jmcb <joelsgp@protonmail.com>
2024-03-22 19:39:15 +0000

    Start screenshot backup tool

---
+++ screenshots-backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+
+# full of images like screenshotXX.bmp
+bootstrap='_nds/nds-bootstrap/screenshots.tar'
+# these images will be randomly displayed
+# on the top screen.
+# overrides displaying screenshots
+wallpapers='_nds/TWiLightMenu/dsimenu/photos'
