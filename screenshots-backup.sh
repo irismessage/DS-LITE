f1bcd3a6777cdf86d0c518f6db4fbccaed57f73b
jmcb <joelsgp@protonmail.com>
2024-03-23 17:01:28 +0000

    Screenshots backup work

--- screenshots-backup.sh
+++ screenshots-backup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -eux
 
-# the empty images have these
-# todo does the firmware store a pointer to the first empt one?
+# the empty images have these as hash
+# they're just all 0 bytes for the same size as a real screenshot
+# nds-bootstrap doesn't store a pointer to this, it just checks magic bytes
+# see here:
+# https://github.com/DS-Homebrew/nds-bootstrap/blob/v1.4.0/retail/cardengine/arm9/source/cardengine.c#L338
 MD5_EMPTY_BMP='ada747d43e8d4f442f59b255fcf969ea'
 # full of images like screenshotXX.bmp
 bootstrap='_nds/nds-bootstrap/screenshots.tar'
@@ -10,8 +13,8 @@ bootstrap='_nds/nds-bootstrap/screenshots.tar'
 # on the top screen.
 # overrides displaying screenshots
 wallpapers='_nds/TWiLightMenu/dsimenu/photos'
-sd='/run/media/joelm/SANDISK/'
-backups='screenshots/'
+sd='/run/media/joelm/SANDISK'
+backups='screenshots'
 
 screenshots_tar="${sd}/${bootstrap}"
 tar_creation_date=$(stat --format='%w' "${screenshots_tar}")
@@ -21,5 +24,14 @@ extract_dest="${backups}/${tar_date_formatted}"
 mkdir -p "${extract_dest}"
 bsdtar --extract --file "${screenshots_tar}" --directory "${extract_dest}"
 
-# todo remove empty files
-# todo convert to png for storage space
+for screenshot_bmp in "${extract_dest}"/*.bmp
+do
+    screenshot_png="${screenshot_bmp%%.bmp}.png"
+    # convert to png, break if reached empty screenshot
+    # if [ ! $(convert "${screenshot_bmp}" "${screenshot_png}") ]
+    # then
+    #     break
+    # fi
+    convert "${screenshot_bmp}" "${screenshot_png}" || true
+done
+rm "${extract_dest}"/*.bmp
