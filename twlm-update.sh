7d9a21403b762c3e259501a5cf472f684c4f3a53
jmcb <joelsgp@protonmail.com>
2024-04-24 17:36:13 +0100

    Fix rsync behaivour

--- twlm-update.sh
+++ twlm-update.sh
@@ -32,14 +32,18 @@ bsdtar \
     --directory "${twlm_new}" \
     '_nds' 'BOOT.NDS'
 
+
+# trailing slashes in src path are needed for correct behaivour
+# see rsync manual
+
 # copy firmware from new to working based on checksums
 # this is fast since it happens on the pc
 # and it creates a directory with accurate modification times
 # based on which files are changed in the update
-rsync -avP --checksum "${twlm_new}" "${twlm_working}"
+rsync -avP --checksum "${twlm_new}/" "${twlm_working}"
 
 # copy firmware from working to sd card
 # this is fast since all it reads from the sd card
 # is size and update time metadata
 # and it only sends modified files since the last update
-rsync -avP "${twlm_working}" "${mountpoint}"
+rsync -avP "${twlm_working}/" "${mountpoint}"
