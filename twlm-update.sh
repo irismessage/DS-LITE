f6783669f2205dbaf3e66b58d62c50464b95ba5d
jmcb <joelsgp@protonmail.com>
2024-04-24 17:12:07 +0100

    Make variable name more sensical

--- twlm-update.sh
+++ twlm-update.sh
@@ -3,7 +3,7 @@ set -eux
 
 firmware_dir='firmware'
 zip="${firmware_dir}/TWiLightMenu-DSi.7z"
-twlm_old="${firmware_dir}/twlm/old"
+twlm_working="${firmware_dir}/twlm/working"
 twlm_new="${firmware_dir}/twlm/new"
 
 disklabel='SANDISK'
@@ -25,14 +25,14 @@ bsdtar \
     --directory "${twlm_new}" \
     '_nds' 'BOOT.NDS'
 
-# copy firmware from new to old based on checksums
+# copy firmware from new to working based on checksums
 # this is fast since it happens on the pc
 # and it creates a directory with accurate modification times
 # based on which files are changed in the update
-rsync -avP --checksum "${twlm_new}" "${twlm_old}"
+rsync -avP --checksum "${twlm_new}" "${twlm_working}"
 
-# copy firmware from old to sd card
+# copy firmware from working to sd card
 # this is fast since all it reads from the sd card
 # is size and update time metadata
 # and it only sends modified files since the last update
-rsync -avP "${twlm_old}" "${mountpoint}"
+rsync -avP "${twlm_working}" "${mountpoint}"
