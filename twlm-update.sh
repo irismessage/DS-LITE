a162335cd55d3bb356733685724c009b79185261
imcb <irismessage@protonmail.com>
2026-01-10 23:06:11 +0000

    update mode toggle

--- twlm-update.sh
+++ twlm-update.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -eux
 
-firmware_dir='firmware'
-zip="${firmware_dir}/TWiLightMenu-DSi.7z"
-twlm_working="${firmware_dir}/twlm/working"
-twlm_new="${firmware_dir}/twlm/new"
-
 disklabel='SANDISK'
 mountpoint="/run/media/${USER}/${disklabel}"
+
+firmware_dir='firmware'
+zip="${firmware_dir}/TWiLightMenu-DSi.7z"
+
+twlm_working="${firmware_dir}/twlm/working"
+twlm_new="${firmware_dir}/twlm/new"
 
 # guide says to copy over _nds/ and BOOT.NDS
 # https://wiki.ds-homebrew.com/twilightmenu/updating-dsi.html
@@ -18,35 +19,38 @@ mountpoint="/run/media/${USER}/${disklabel}"
 #   however we don't want to replace it on update and overwrite settings
 # version.txt is just the version number
 
-# dumb overwrite
-# bsdtar \
-#     --extract --file "${zip}" \
-#     --directory "${mountpoint}" \
-#     '_nds' 'BOOT.NDS'
-# exit
+DUMB=1
+if [ "${DUMB}" -eq 1 ]; then
+    # dumb overwrite
+    bsdtar \
+        --extract --file "${zip}" \
+        --directory "${mountpoint}" \
+        '_nds' 'BOOT.NDS'
+    exit
+else
+    # incremental update
+    # extract firmware zip to new
+    mkdir -p "${twlm_new}"
+    bsdtar \
+        --extract --file "${zip}" \
+        --directory "${twlm_new}" \
+        '_nds' 'BOOT.NDS' 'version.txt'
 
-# incremental update
-# extract firmware zip to new
-mkdir -p "${twlm_new}"
-bsdtar \
-    --extract --file "${zip}" \
-    --directory "${twlm_new}" \
-    '_nds' 'BOOT.NDS' 'version.txt'
 
+    # trailing slashes in src path are needed for correct behaivour
+    # see rsync manual
 
-# trailing slashes in src path are needed for correct behaivour
-# see rsync manual
+    # copy firmware from new to working based on checksums
+    # this is fast since it happens on the pc
+    # and it creates a directory with accurate modification times
+    # based on which files are changed in the update
+    # todo fix: -a (archive) preserves modification time
+    #   when that's the opposite of what I want to do
+    rsync -avP --checksum "${twlm_new}/" "${twlm_working}"
 
-# copy firmware from new to working based on checksums
-# this is fast since it happens on the pc
-# and it creates a directory with accurate modification times
-# based on which files are changed in the update
-# todo fix: -a (archive) preserves modification time
-#   when that's the opposite of what I want to do
-rsync -avP --checksum "${twlm_new}/" "${twlm_working}"
-
-# copy firmware from working to sd card
-# this is fast since all it reads from the sd card
-# is size and update time metadata
-# and it only sends modified files since the last update
-rsync -avP "${twlm_working}/" "${mountpoint}"
+    # copy firmware from working to sd card
+    # this is fast since all it reads from the sd card
+    # is size and update time metadata
+    # and it only sends modified files since the last update
+    rsync -avP "${twlm_working}/" "${mountpoint}"
+fi
